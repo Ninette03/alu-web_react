de413895879ab06ece389007c5395290eabc973b
Ninette03 <n.agatesi@alustudent.com>
2026-01-06 10:24:29 +0200

    Task 0

---
+++ Typescript/README.md
@@ -0,0 +1,1 @@
+Creating interfaces in Typescript
---
+++ Typescript/task_0/js/main.js
@@ -0,0 +1,13 @@
+var student1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    location: "New York"
+};
+var student2 = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 21,
+    location: "New York"
+};
+var studentsList = [student1, student2];
