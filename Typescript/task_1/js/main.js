385c978c5257694e451691c3a77a112bddad3cd3
Ninette03 <n.agatesi@alustudent.com>
2026-01-07 10:33:35 +0200

    Task 2

--- Typescript/task_1/js/main.js
+++ Typescript/task_1/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var teacher3 = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -6,3 +8,11 @@ var teacher3 = {
     contract: false,
 };
 console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
