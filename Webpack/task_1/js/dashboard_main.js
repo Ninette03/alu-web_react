38f846765a0fcc79e921201d795fe9c2a6d6847d
Ninette03 <n.agatesi@alustudent.com>
2026-01-09 15:55:53 +0200

    Task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('#count').text(`${count} clicks on the button`);
+const debouncedUpdate = debounce(updateCounter, 500);
+$('button').on('click', debouncedUpdate);
